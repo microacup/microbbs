645599be7279df9153ad94773b29ba7f18f0db43
microacup <xiangmain@gmail.com>
2016-11-22 11:39:16 +0800

    手动404

--- api/src/main/java/me/micro/bbs/MicrobbsApplication.java
+++ api/src/main/java/me/micro/bbs/MicrobbsApplication.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 import javax.annotation.PostConstruct;
 
@@ -23,6 +27,18 @@ public class MicrobbsApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(MicrobbsApplication.class);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> {
+			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/404");
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/404");
+
+			container.addErrorPages(error401Page, error404Page, error500Page);
+		});
 	}
 
 	@PostConstruct
